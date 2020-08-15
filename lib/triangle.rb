25863e41b7fde216a0f895372896e0d9605fb235
George Talnagi <gtalnagi@yahoo.com>
2020-08-15 00:28:03 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+  
+  def initialize(side_1, side2, side3)
+    @side_1 = side_1
+    @side_2 = side_2 
+    @side_3 = side_3
+  end 
+  
+  def kind
+    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
+       raise TriangleError
+  end
+end 
+  
+  class TriangleError < StandardError
+ 
+  end 
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
