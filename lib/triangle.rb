668410d76f83af55c820df039fcf3c05de221df6
George Talnagi <gtalnagi@yahoo.com>
2020-08-15 00:56:10 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,9 +7,9 @@ class Triangle
 
   def kind()
     if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
-      puts TriangleError
+      TriangleError
     elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
-      puts TriangleError
+      TriangleError
     else
       if (@side_1 == @side_2) && (@side_2 == @side_3)
         :equilateral
